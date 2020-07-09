11647b19eba840802083eb518e8937099d224cf3
Gabriel Iriarte <gabo.iriarte@icloud.com>
2020-07-08 22:06:04 -0400

    Migracion de tablas predeterminadas realizadas

---
+++ database/migrations/2020_07_09_013833_create_trabajos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrabajosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trabajos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trabajos');
+    }
+}
