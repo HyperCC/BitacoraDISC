bfdc68b5d16d5ccaa67589bef6560a2642e1fd1e
Gabriel Iriarte <gabo.iriarte@icloud.com>
2020-07-16 19:13:53 -0400

    Edit en bitacoras se muestra

--- resources/views/bitacorass/edit.blade.php
+++ resources/views/bitacorass/edit.blade.php
@@ -1,31 +1,26 @@
-<!-- @extends('helpers.template')
-@section('content')
-
+@extends('helpers.template')
+@section('title_head', 'Editar Bitacora')
+@section('content_body')
 <div class="container">
-@include('helpers.validate_errors')
+  @include('helpers.validate_errors')
 
-            <form class="bg-white py-3 px-4 shadow rounded" method="POST"
-               action="{{ route('bitacoras-update', $bitacora) }}">
-                 @csrf
-                   @method('PATCH')
-                     <h2 class="display-4">Editar trabajos</h2>
-                       <hr>
+  <div>
+    <form class="bg-white py-3 px-4 shadow rounded" method="POST" action="{{ route('bitacoras-update', $bitacora) }}">
+      @csrf
+      @method('PATCH')
+      <h2 class="display-4">Editar trabajos</h2>
+      <hr>
 
-                            <div class="form-group">
-                          <label for="titulo"> Nombre </label>
-                          <input class="form-control shadow-sm" name="name" type="text"
-                       value="{{ old('name', $bitacora->name) }}">
-                       </div>
-     
-                            </div>
-                         </div>
-                      </div>
+      <div class="form-group">
+        <label for="titulo"> Nombre </label>
+        <input class="form-control shadow-sm" name="name" type="text" value="{{ old('name', $bitacora->name) }}">
+      </div>
 
-                    </div>
-                  </div>
-                 <button class="btn btn-primary btn-lg btn-block" type="submit"> Guardar</button>
-                 <a class="btn btn-link btn-block" href="{{ route('users-index') }}"> Cancelar </a>
 
-              </form>
-   </div>
-@endsection  -->
+
+      <button class="btn btn-primary btn-lg btn-block" type="submit"> Guardar</button>
+      <a class="btn btn-link btn-block" href="{{ route('users-index') }}"> Cancelar </a>
+
+    </form>
+  </div>
+  @endsection
