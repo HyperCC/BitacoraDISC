f25e16986f32f4f1022819f9dc83447722bcaa2b
Michel Campos <michellalexander2000@gmail.com>
2020-08-27 03:55:03 -0400

    Ahora se avisa que no se tiene avances si se desea poner una evidencia a una bitacora sin avances

--- resources/views/evidenciaOperations/create.blade.php
+++ resources/views/evidenciaOperations/create.blade.php
@@ -1,82 +1,97 @@
+
+
 @extends('helpers.template')
 
 @section('title_head', 'Crear Avance')
 
 @section('content_body')
 
+
+
+
+
     <div class="container">
 
         <div class="row">
 
             <div class="col-12 col-sm-10 col-lg-6 mx-auto my-3">
+                
                 @if(\Illuminate\Support\Facades\Auth::user()->disponibilidad == 'Si')
                     <h1 class="text-center">No tienes bitácoras inscritas.</h1>
+                
                 @elseif(\Illuminate\Support\Facades\Auth::user()->rol == 'Estudiante' & \Illuminate\Support\Facades\Auth::user()->disponibilidad == 'No'  & auth()->user()->bitacoras->first()->estado != 'Finalizada' )
 
                     @foreach(auth()->user()->bitacoras as $bita)
 
                         @if(($bita->estado)=='Activa')
 
-                            <form class="bg-white py-3 px-4 shadow rounded" method="POST"
-                                  action="{{ route('evidencia-store') }}"
-                                  enctype="multipart/form-data">
+                            @if(\Illuminate\Support\Facades\DB::select('SELECT bitacora_id FROM avances WHERE bitacora_id = ?',[$bita->id]))
 
-                                @csrf
+                                <form class="bg-white py-3 px-4 shadow rounded" method="POST"
+                                    action="{{ route('evidencia-store') }}"
+                                    enctype="multipart/form-data">
 
-                                <input type="hidden" name="name_user" value="{{auth()->user()->name}}">
+                                    @csrf
 
-                                <tbody>
-                                <!-- Arreglar forelse para poder obtener la bitacora del usuario logueado -->
+                                    <input type="hidden" name="name_user" value="{{auth()->user()->name}}">
 
-                                </tbody>
+                                    <tbody>
+                                    <!-- Arreglar forelse para poder obtener la bitacora del usuario logueado -->
 
-                                <div class="text-center">
-                                    <span class="display-3">Crear una Evidencia</span>
-                                </div>
+                                    </tbody>
 
-                                <hr>
-
-                                <div class="form-group">
-                                    <label for="name_evid"> Nombre Evidencia </label>
-                                    <input class="form-control shadow-sm bg-light" name="name_evid" type="text">
-                                </div>
-
-
-                                @foreach(auth()->user()->bitacoras as $bita)
-                                    <div class="form-group">
-                                        <label for="avance_id"> Avances en la Bitacora </label>
-
-                                        <select class="form-control shadow-sm custom-select" name="avance_id">
-                                            @foreach($bita->avances as $avance)
-                                                <option value="{{$avance->id}}">
-                                                    {{$avance->nombre}} ({{$avance->created_at}})
-                                                </option>
-                                            @endforeach
-                                        </select>
-
+                                    <div class="text-center">
+                                        <span class="display-3">Crear una Evidencia</span>
                                     </div>
-                                @endforeach
 
-                                <div class="form-group">
-                                    <label for="archivo"> Adjuntar evidencia </label>
-                                    <br>
-                                    <input accept="image/jpg, image/png, application/pdf, .docx" class="" name="archivo"
-                                           type="file" required>
+                                    <hr>
+
+                                    <div class="form-group">
+                                        <label for="name_evid"> Nombre Evidencia </label>
+                                        <input class="form-control shadow-sm bg-light" name="name_evid" type="text">
+                                    </div>
+
+
+                                    @foreach(auth()->user()->bitacoras as $bita)
+                                        <div class="form-group">
+                                            <label for="avance_id"> Avances en la Bitacora </label>
+
+                                            <select class="form-control shadow-sm custom-select" name="avance_id">
+                                                @foreach($bita->avances as $avance)
+                                                    <option value="{{$avance->id}}">
+                                                        {{$avance->nombre}} ({{$avance->created_at}})
+                                                    </option>
+                                                @endforeach
+                                            </select>
+
+                                        </div>
+                                    @endforeach
+
+                                    <div class="form-group">
+                                        <label for="archivo"> Adjuntar evidencia </label>
+                                        <br>
+                                        <input accept="image/jpg, image/png, application/pdf, .docx" class="" name="archivo"
+                                            type="file" required>
+                                    </div>
+
+                                    <hr>
+
+                                    <div class="py-3">
+                                        <button type="submit" class="btn btn-primary btn-lg btn-block rounded-pill">
+                                            Adjuntar Evidencia
+                                        </button>
+                                        <a class="btn btn-lg btn-block btn-outline-dark rounded-pill"
+                                        href="{{route('home')}}">
+                                            Cancelar
+                                        </a>
+                                    </div>
+                                </form>
+                            @else
+                                <div class="display-3 text-center">
+                                    <p>No tiene avances al cual añadir una evidencia.</p>
                                 </div>
 
-                                <hr>
-
-                                <div class="py-3">
-                                    <button type="submit" class="btn btn-primary btn-lg btn-block rounded-pill">
-                                        Adjuntar Evidencia
-                                    </button>
-                                    <a class="btn btn-lg btn-block btn-outline-dark rounded-pill"
-                                       href="{{route('home')}}">
-                                        Cancelar
-                                    </a>
-                                </div>
-                            </form>
-
+                            @endif
                         @else
                             @if(($bita->causa_renuncia)=='Aprobación del término de trabajo')
                                 <div class="mx-auto">
